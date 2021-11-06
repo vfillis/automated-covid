c88e9608dc01146e13eae969e8307555a16c7577
Vanessa Fillis <v.fillis1995@gmail.com>
2021-11-06 10:25:34 +0000

    Update england-covid-cases.R

--- R/england-covid-cases.R
+++ R/england-covid-cases.R
@@ -266,3 +266,88 @@ healthcare.England <- healthcare.England %>%
   dplyr::mutate(`Seven-day average hospital cases` = zoo::rollmean(`Daily people in hospital with coronavirus`, k = 7, align="left", fill = NA)) 
 
 write.csv(healthcare.England, file = "data/healthcare-England.csv")
+
+# Covid vaccinations in England: people vaccinated each day
+
+#' Extracts paginated data by requesting all of the pages
+#' and combining the results.
+#'
+#' @param filters    API filters. See the API documentations for 
+#'                   additional information.
+#'                   
+#' @param structure  Structure parameter. See the API documentations 
+#'                   for additional information.
+#'                   
+#' @return list      Comprehensive list of dictionaries containing all 
+#'                   the data for the given ``filter`` and ``structure`.`
+get_paginated_data <- function (filters, structure) {
+  
+  endpoint     <- "https://api.coronavirus.data.gov.uk/v1/data"
+  results      <- list()
+  current_page <- 1
+  
+  repeat {
+    httr::GET(
+      url   = endpoint,
+      query = list(
+        filters   = paste(filters, collapse = ";"),
+        structure = jsonlite::toJSON(structure, auto_unbox = TRUE),
+        page      = current_page
+      ),
+      timeout(10)
+    ) -> response
+    
+    # Handle errors:
+    if ( response$status_code >= 400 ) {
+      err_msg = httr::http_status(response)
+      stop(err_msg)
+    } else if ( response$status_code == 204 ) {
+      break
+    }
+    
+    # Convert response from binary to JSON:
+    json_text <- content(response, "text")
+    dt        <- jsonlite::fromJSON(json_text)
+    results   <- rbind(results, dt$data)
+    
+    if ( is.null( dt$pagination$`next` ) ){
+      break
+    }
+    
+    current_page <- current_page + 1;
+  }
+  
+  return(results)
+  
+}
+# Create filters:
+query_filters <- c(
+  "areaType=nation",
+  "areaName=England"
+)
+# Create the structure as a list or a list of lists:
+query_structure <- list(
+  date       = "date", 
+  name       = "areaName",
+  #code       = "areaCode"
+  vaccination.first = "newPeopleVaccinatedFirstDoseByPublishDate",
+  vaccination.second = "newPeopleVaccinatedSecondDoseByPublishDate",
+  vaccination.third = "newPeopleVaccinatedThirdInjectionByPublishDate"
+  
+)
+vaccinations.England <- get_paginated_data(query_filters, query_structure)
+list(
+  "Shape"                = dim(vaccinations.England),
+  "Data (first 3 items)" = vaccinations.England[0:3, 0:-1]
+) -> report
+print(report)
+
+#Rename columns for better distinction
+vaccinations.England <- vaccinations.England %>%
+  rename(
+    `First dose` = vaccination.first,
+    `Second dose` = vaccination.second,
+    `Third dose` = vaccination.third
+  )
+
+write.csv(vaccinations.England, file = "data/vaccinations-England.csv")
